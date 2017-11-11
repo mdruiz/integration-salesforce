4f4607ee012cc375919e0d39f694741550a134f1
fred belotte <fred@belotte.xyz>
2017-11-11 13:08:56 -0500

    add the docker package repository.

--- Vagrantup.sh
+++ Vagrantup.sh
@@ -12,3 +12,9 @@ apt install -y \
   ca-certificates \
   curl \
   software-properties-common
+
+# DOCKER - REPOSITORY
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable"
