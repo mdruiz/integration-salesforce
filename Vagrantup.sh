fc48be96c1ca47aade1bb36438de6845fb73a9ec
fred belotte <fred@belotte.xyz>
2018-02-05 22:18:56 -0500

    add the docker compose install script.

--- Vagrantup.sh
+++ Vagrantup.sh
@@ -23,6 +23,12 @@ apt update
 apt install -y \
   docker-ce=$DOCKER_CE_VERSION
 
+# DOCKER - COMPOSE TOOLING
+apt update
+curl -L https://github.com/docker/compose/releases/download/$DOCKER_CT_VERSION/docker-compose-`uname -s`-`uname -m` \
+  -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # DOCKER - PERMISSION
 usermod -aG docker vagrant
 shutdown -r 0
