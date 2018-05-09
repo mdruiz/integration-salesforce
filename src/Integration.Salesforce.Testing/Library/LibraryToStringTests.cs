7bd1c109c55d8d745c4f7425be875651bc479283
derek white <derekbwhite80@gmail.com>
2018-05-09 16:49:00 -0400

    add person tostring test [#157264138]

---
+++ src/Integration.Salesforce.Testing/Library/LibraryToStringTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Integration.Salesforce.Library.Models;
+using Xunit;
+
+namespace Integration.Salesforce.Testing.Library.ModelTests
+{
+    public class PersonToStringTests
+    {
+        ModelData md = new ModelData();
+
+        [Fact]
+        public void ContactToString_ReturnsValidString_True()
+        {
+            // assemble
+            Person person = md.Person();
+            Address address = md.Address();
+            // act
+            var response = person.ToString();
+            var teststring = $"PERSON{{Name:{person.FirstName} {person.LastName};Phone:{person.Phone};EMail:{person.EMail};Role:{person.Role};HasCar:{person.HasCar};BatchName:{person.BatchName};}}";
+            teststring += address.ToString();
+            // assert
+            Assert.NotNull(response);
+            Assert.Equal(teststring, response, true);
+        }
+    }
+}
