7e8cb59adfcdd2ac1a4b03079dfe708c483780df
Josh <jhendrix6@cub.uca.edu>
2018-05-11 15:00:08 -0400

    Gave address and person map functions, tested validity [#157297480]

--- src/Integration.Salesforce.Library/Models/Address.cs
+++ src/Integration.Salesforce.Library/Models/Address.cs
@@ -1,36 +1,45 @@
 using System.ComponentModel.DataAnnotations;
 using Integration.Salesforce.Library.Abstract;
 using Integration.Salesforce.Library.Validation;
+using Newtonsoft.Json.Linq;
 
 namespace Integration.Salesforce.Library.Models
 {
     public class Address : AbstractAddress
     {
-        [Required]
-        [StringValidation(ErrorMessage = "{0} invalid string input")]
-        public override string StreetAddress { get; set; }
+      [Required]
+      [StringValidation(ErrorMessage = "{0} invalid string input")]
+      public override string StreetAddress { get; set; }
 
-        [Required]
-        [StringValidation(ErrorMessage = "{0} invalid string input")]
-        public override string City { get; set; }
+      [Required]
+      [StringValidation(ErrorMessage = "{0} invalid string input")]
+      public override string City { get; set; }
 
-        [Required]
-        [MaxLength(2)]
-        [MinLength(2)]
-        [StringValidation(ErrorMessage = "{0} invalid string input")]
-        public string State { get; set; }
+      [Required]
+      [MaxLength(2)]
+      [StringValidation(ErrorMessage = "{0} invalid string input")]
+      public string State { get; set; }
 
-        [Required]
-        [MaxLength(5)]
-        [MinLength(5)]
-        [NumberValidation(ErrorMessage = "{0} invalid number input")]
-        public override int Zip { get; set; }
+      [Required]
+      [NumberValidation(ErrorMessage = "{0} invalid number input")]
+      public override int Zip { get; set; }
+      [Required]
+      [StringValidation(ErrorMessage = "{0} invalid string input")]
+      public string Country {get;set;}
 
-        public override string ToString()
-        {
-            string returnString = $"ADDRESS{{StreetAddress:{StreetAddress};City:{City};State:{State};Zip:{Zip};}}";
+      public void MapJsonToModel(JObject json)
+      {
+        //Address (Street, city, state, postal code, country)
+        this.StreetAddress = json["MailingStreet"].ToString();
+        this.City = json["MailingCity"].ToString();
+        this.State = json["MailingState"].ToString();
+        this.Zip = json["MailingPostalCode"].ToObject<int>();
+        this.Country = json["MailingCountry"].ToString();
+      }
 
-            return returnString;
-        }
+      public override string ToString()
+      {
+        return $"{StreetAddress}, {City}, {State}, {Zip}";
+      }
     }
 }
