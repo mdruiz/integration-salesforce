992f82e7ae50786d9f80d759ad23c9db2871ddde
Josh <jhendrix6@cub.uca.edu>
2018-05-14 14:12:30 -0400

    Added Person Controller, get all people and delete a person [#157254644]

---
+++ src/Integration.Salesforce.Service/Controllers/PersonController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Integration.Salesforce.Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Integration.Salesforce.Context;
+using Microsoft.Extensions.Options;
+
+namespace Integration.Salesforce.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        private IOptions<Settings> _settings;
+        private DbContext<Person> context;
+        public PersonController(IOptions<Settings> settings)
+        {
+            _settings = settings;
+            context = new DbContext<Person>(_settings);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Person>> GetAllPersons()
+        {
+            return await Task.Run(() => context.ReadMongoEntries());
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(string id)
+        {
+            Person p = context.GetEntryByID(id);
+            p.Active = false;
+            List<Person> pList = new List<Person>();
+            pList.Add(p);
+            context.UpdateMongoEntries(pList);
+            return await Task.Run(() => Ok());
+        }
+    }
+
+}
